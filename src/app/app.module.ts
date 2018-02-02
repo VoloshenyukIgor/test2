686b75310bb581486d696996c209f33396a522e5
VoloshenyukIgor <voloshenyukigor92@gmail.com>
2018-02-02 23:45:04 +0200

    2. work 70%/30%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { InputComponent } from './input/input.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientJsonpModule, HttpClientModule} from '@angular/common/http';
+import {UsersService} from './users.service';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    InputComponent
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule
+
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
